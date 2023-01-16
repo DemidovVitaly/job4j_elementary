85f549ddc148822ff905578d9e864683db62c0d7
DemidovVitaly <yademidov011@yandex.ru>
2023-01-16 22:25:16 +0300

    1.2. Входные данные в тесте [#297750 #381463] - исправление, добавление пробелов между методами

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -64,4 +64,15 @@ public class X2Test {
         int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
     }
+
+    @Test
+    public void whenA1B1C1X2Then7() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 2;
+        int expected = 7;
+        int rsl = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, rsl);
+    }
 }
