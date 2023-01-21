a9e25c4b6f6dab2c72e3a0da8e1663ad75ae1da0
DemidovVitaly <yademidov011@yandex.ru>
2023-01-22 00:56:42 +0300

    4.6.1. Switch [#334070 #383629] исправление замечаний бота

--- src/test/java/ru/job4j/condition/SwitchWeekTest.java
+++ src/test/java/ru/job4j/condition/SwitchWeekTest.java
@@ -3,9 +3,7 @@ package ru.job4j.condition;
 import org.junit.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
-
 public class SwitchWeekTest {
-
     @Test
     public void when1isMonday() {
         int day = 1;
@@ -16,7 +14,7 @@ public class SwitchWeekTest {
 
     @Test
     public void when5isFriday() {
-        int day =5;
+        int day = 5;
         String result = SwitchWeek.nameOfDay(day);
         String expected = "Friday";
         assertThat(result).isEqualTo(expected);
