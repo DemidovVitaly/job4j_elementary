ab14cac59e465cf79d92c4c1f1078422a7b949c3
DemidovVitaly <yademidov011@yandex.ru>
2023-01-22 00:29:38 +0300

    4.6.2. Switch - matcher [#334071]

---
+++ src/test/java/ru/job4j/condition/MultipleSwitchWeekTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class MultipleSwitchWeekTest {
+
+    @Test
+    public void whenMondayIs1() {
+        String day = "Monday";
+        int result = MultipleSwitchWeek.numberOfDay(day);
+        int expected = 1;
+        assertThat(result).isEqualTo(expected);
+    }
+    @Test
+    public void whenWednesdayIs3() {
+        String day = "Среда";
+        int result = MultipleSwitchWeek.numberOfDay(day);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+    @Test
+    public void whenError() {
+        String day = "nothing";
+        int result = MultipleSwitchWeek.numberOfDay(day);
+        int expected = -1;
+        assertThat(result).isEqualTo(expected);
+    }
+}
