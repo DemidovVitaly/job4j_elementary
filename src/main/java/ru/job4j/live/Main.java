cba3b3c2d60d9cceb3f874bb13c459401debe324
DemidovVitaly <yademidov011@yandex.ru>
2023-01-25 06:04:08 +0300

    6.1. Генерация конструкций в IDEA [#279134 #129579]

---
+++ src/main/java/ru/job4j/live/Main.java
@@ -0,0 +1,11 @@
+package ru.job4j.live;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Привет!!!");
+        for (int i = 0; i < 11; i++) {
+            System.out.print(i + "-");
+        }
+        System.out.println();
+    }
+}
