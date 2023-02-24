796e0d5e3f32bb2e8676053d187d66fca2b84ea4
DemidovVitaly <yademidov011@yandex.ru>
2023-02-24 22:14:19 +0300

    3. Перегрузить метод max для трех чисел [#122644 #129642]

--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -31,4 +31,25 @@ public class MaxTest {
         int expected = 2;
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    public void whenMax1To2To3Then3() {
+        int first = 1;
+        int second = 2;
+        int third = 3;
+        int result = Max.max(first, second, third);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenMax1To2To3To4Then4() {
+        int first = 1;
+        int second = 2;
+        int third = 3;
+        int fourth = 4;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 4;
+        assertThat(result).isEqualTo(expected);
+    }
 }
--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -3,48 +3,42 @@ package ru.job4j.condition;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.awt.*;
+
 public class PointTest {
     @Test
     public void when00to20then2() {
         double expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point one = new Point(0, 0);
+        Point two = new Point(0, 2);
+        double out = one.distance(two);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when20to24then2() {
         double expected = 4;
-        int x1 = 2;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 4;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point one = new Point(2, 0);
+        Point two = new Point(2, 4);
+        double out = one.distance(two);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when11to33then2dot83() {
         double expected = 2.83;
-        int x1 = 1;
-        int y1 = 1;
-        int x2 = 3;
-        int y2 = 3;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point one = new Point(1, 1);
+        Point two = new Point(3, 3);
+        double out = one.distance(two);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when22to00then2dot83() {
         double expected = 2.83;
-        int x1 = 2;
-        int y1 = 2;
-        int x2 = 0;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point one = new Point(2, 2);
+        Point two = new Point(0, 0);
+        double out = one.distance(two);
         Assert.assertEquals(expected, out, 0.01);
     }
 }
